df424f8bd0d5d4a889b4b948ace9c194e8f2a6eb
Xiaoyan Wang <xiaoyan.w.65@gmail.com>
2022-03-17 06:23:43 -0700

    gcs

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "google" {
   credentials = file("terraform-key.json")
-
   project = "terraform-course-344403"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
