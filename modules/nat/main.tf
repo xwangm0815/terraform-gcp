ab94f9d6f69603d617310edd565f14c4280ab07d
Xiaoyan Wang <xiaoyan.w.65@gmail.com>
2022-03-31 10:32:29 +0000

    Add module NAT gateway

---
+++ modules/nat/main.tf
@@ -0,0 +1,13 @@
+resource "google_compute_router" "nat" {
+  name    = "${var.nat_router_name}"
+  region  = "${var.region}"
+  network = "${var.network_name}"
+}
+
+resource "google_compute_router_nat" "config" {
+  name                               = "${var.nat_router_name}-config"
+  router                             = "${google_compute_router.nat.name}"
+  region                             = "${var.region}"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
---
+++ modules/nat/variables.tf
@@ -0,0 +1,14 @@
+variable "network_name" {
+  type        = string
+  description = "vpc network name"
+}
+
+variable "nat_router_name" {
+  type        = string
+  description = "NAT router name"
+  default     = ""
+}
+
+variable "region" {
+  description = "The GCP region where the  resides."
+}
